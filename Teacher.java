5a99a35e9642a7dbff19b244bfdd391ca13b041b
usii25 <karimatuzfirdausi@gmail.com>
2024-09-02 15:40:25 +0700

    Update Teacher.java

--- Teacher.java
+++ Teacher.java
@@ -3,19 +3,18 @@ package CommunityMemberCase;
  * @author Nama : Karimatuz Zahrah Firdausi
             NIM : 09020623029
  */
+/*Membuat class Teacher yang menjadi child class dr class Faculty
+  Pemilihan atribut disesuaikan dengan kebutuhan
+*/
 public class Teacher extends Faculty{
     private String bidangMengajar;
 
     /**
-     * @return the bidangMengajar
+     * Membuat method untuk memasukkan dan mengeluarkan nilai
      */
     public String getBidangMengajar() {
         return bidangMengajar;
     }
-
-    /**
-     * @param bidangMengajar the bidangMengajar to set
-     */
     public void setBidangMengajar(String bidangMengajar) {
         this.bidangMengajar = bidangMengajar;
     }
