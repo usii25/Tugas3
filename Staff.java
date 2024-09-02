027eb62db349a7f3453a54a230b527ed13b3d157
usii25 <karimatuzfirdausi@gmail.com>
2024-09-02 15:27:17 +0700

    Update Staff.java

--- Staff.java
+++ Staff.java
@@ -3,19 +3,18 @@ package CommunityMemberCase;
  * @author Nama : Karimatuz Zahrah Firdausi
             NIM : 09020623029
  */
+/*Membuat class Staff yang menjadi child class dr class Employee
+  Pemilihan atribut disesuaikan dengan kebutuhan
+*/
 public class Staff extends Employee {
     private String departemen;
 
     /**
-     * @return the departemen
+     * Membuat method untuk memasukkan dan mengeluarkan nilai
      */
     public String getDepartemen() {
         return departemen;
     }
-
-    /**
-     * @param departemen the departemen to set
-     */
     public void setDepartemen(String departemen) {
         this.departemen = departemen;
     }
