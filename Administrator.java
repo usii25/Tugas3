0313f745c2b27ab5e5b30e288a7e214ded79a003
usii25 <karimatuzfirdausi@gmail.com>
2024-09-02 15:36:36 +0700

    Update Administrator.java

--- Administrator.java
+++ Administrator.java
@@ -3,19 +3,18 @@ package CommunityMemberCase;
  * @author Nama : Karimatuz Zahrah Firdausi
             NIM : 09020623029
  */
+/*Membuat class Administrator yang menjadi child class dr class Faculty
+  Pemilihan atribut disesuaikan dengan kebutuhan
+*/
 public class Administrator extends Faculty{
-    private String tanggungJawab;
+    private String TanggungJawab;
 
     /**
-     * @return the tanggungJawab
+     * Membuat method untuk memasukkan dan mengeluarkan nilai
      */
     public String getTanggungJawab() {
         return tanggungJawab;
     }
-
-    /**
-     * @param tanggungJawab the tanggungJawab to set
-     */
     public void setTanggungJawab(String tanggungJawab) {
         this.tanggungJawab = tanggungJawab;
     }
