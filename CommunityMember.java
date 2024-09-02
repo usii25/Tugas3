96e2cc0889135adf0bc61b7bcb625b4986a21870
usii25 <karimatuzfirdausi@gmail.com>
2024-09-02 15:01:59 +0700

    Update CommunityMember.java

--- CommunityMember.java
+++ CommunityMember.java
@@ -3,49 +3,33 @@ package CommunityMemberCase;
  * @author Nama : Karimatuz Zahrah Firdausi
             NIM : 09020623029
  */
+/*Membuat class ComunityMember dengan memiliki atribut nama,alamat dan email.
+  Pemilihan atribut disesuaikan dengan kebutuhan ComunityMember
+*/
 public class CommunityMember {
     private String nama;
     private String alamat;
     private String email;
 
-    /**
-     * @return the nama
+    /*Membuat method untuk memasukkan dan mengeluarkan nilai
      */
     public String getNama() {
         return nama;
     }
-
-    /**
-     * @param nama the nama to set
-     */
     public void setNama(String nama) {
         this.nama = nama;
     }
 
-    /**
-     * @return the alamat
-     */
     public String getAlamat() {
         return alamat;
     }
-
-    /**
-     * @param alamat the alamat to set
-     */
     public void setAlamat(String alamat) {
         this.alamat = alamat;
     }
 
-    /**
-     * @return the email
-     */
     public String getEmail() {
         return email;
     }
-
-    /**
-     * @param email the email to set
-     */
     public void setEmail(String email) {
         this.email = email;
     }
