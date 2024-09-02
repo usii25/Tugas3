ef056a069b79a15d39ed36cba9da7ce370743fc5
usii25 <karimatuzfirdausi@gmail.com>
2024-09-02 15:05:15 +0700

    Update Employee.java

--- Employee.java
+++ Employee.java
@@ -3,6 +3,10 @@ package CommunityMemberCase;
  * @author Nama : Karimatuz Zahrah Firdausi
             NIM : 09020623029
  */
+
+/*Membuat class Employee yang menjadi child class dari class ComunityMember
+  
+*/
 public class Employee extends CommunityMember {
     private String bidang;
     
