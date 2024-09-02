f5462d8526c318f2754401c74a7c8241b733e87a
usii25 <karimatuzfirdausi@gmail.com>
2024-09-02 15:15:07 +0700

    Update Alumnus.java

--- Alumnus.java
+++ Alumnus.java
@@ -3,11 +3,17 @@ package CommunityMemberCase;
  * @author Nama : Karimatuz Zahrah Firdausi
             NIM : 09020623029
  */
+/*Membuat class Alumnus yang menjadi child class dr class ComunityMember
+  Pemilihan atribut disesuaikan dengan kebutuhan
+*/
 public class Alumnus extends CommunityMember{
     private String prodi;
     private int angkatan;
     private int lulusanTahun;
-    
+
+    /*
+     * Membuat method untuk memasukkan dan mengeluarkan nilai
+     */
     public void BelumBekerja(){
         System.out.println("Belum Bekerja");
     }
@@ -15,44 +21,23 @@ public class Alumnus extends CommunityMember{
         System.out.println("Sudah Bekerja");
     }
 
-    /**
-     * @return the prodi
-     */
     public String getProdi() {
         return prodi;
     }
-
-    /**
-     * @param prodi the prodi to set
-     */
     public void setProdi(String prodi) {
         this.prodi = prodi;
     }
 
-    /**
-     * @return the angkatan
-     */
     public int getAngkatan() {
         return angkatan;
     }
-
-    /**
-     * @param angkatan the angkatan to set
-     */
     public void setAngkatan(int angkatan) {
         this.angkatan = angkatan;
     }
 
-    /**
-     * @return the lulusanTahun
-     */
     public int getLulusanTahun() {
         return lulusanTahun;
     }
-
-    /**
-     * @param lulusanTahun the lulusanTahun to set
-     */
     public void setLulusanTahun(int lulusanTahun) {
         this.lulusanTahun = lulusanTahun;
     }
