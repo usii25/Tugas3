ff335108e3c0ee68263845c5c61844605bc2d64c
usii25 <karimatuzfirdausi@gmail.com>
2024-09-02 15:24:33 +0700

    Update Faculty.java

--- Faculty.java
+++ Faculty.java
@@ -3,20 +3,18 @@ package CommunityMemberCase;
  * @author Nama : Karimatuz Zahrah Firdausi
             NIM : 09020623029
  */
+/*Membuat class Faculty yang menjadi child class dr class Employee
+  Pemilihan atribut disesuaikan dengan kebutuhan
+*/
 public class Faculty extends Employee{
     private String tugas;
     
-
     /**
-     * @return the tugas
+     * Mmembuat method untuk memasukkan dan mengeluarkan nilai
      */
     public String getTugas() {
         return tugas;
     }
-
-    /**
-     * @param tugas the tugas to set
-     */
     public void setTugas(String tugas) {
         this.tugas = tugas;
     }
