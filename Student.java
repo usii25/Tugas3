7cf996a18a3f3bea181889af773951985674d5a5
usii25 <karimatuzfirdausi@gmail.com>
2024-09-02 15:10:05 +0700

    Update Student.java

--- Student.java
+++ Student.java
@@ -3,49 +3,34 @@ package CommunityMemberCase;
  * @author Nama : Karimatuz Zahrah Firdausi
             NIM : 09020623029
  */
+/*Membuat class Student yang menjadi child class dr class ComunityMember
+  Pemilihan atribut disesuaikan dengan kebutuhan
+*/
 public class Student extends CommunityMember {
     private String prodi;
     private String fakultas;
     private int semester;
 
     /**
-     * @return the prodi
+     * Membuat method untuk memasukkan dan mengeluarkan nilai
      */
     public String getProdi() {
         return prodi;
     }
-
-    /**
-     * @param prodi the prodi to set
-     */
     public void setProdi(String prodi) {
         this.prodi = prodi;
     }
 
-    /**
-     * @return the fakultas
-     */
     public String getFakultas() {
         return fakultas;
     }
-
-    /**
-     * @param fakultas the fakultas to set
-     */
     public void setFakultas(String fakultas) {
         this.fakultas = fakultas;
     }
 
-    /**
-     * @return the semester
-     */
     public int getSemester() {
         return semester;
     }
-
-    /**
-     * @param semester the semester to set
-     */
     public void setSemester(int semester) {
         this.semester = semester;
     }
